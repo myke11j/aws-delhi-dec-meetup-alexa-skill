980eef868418ef971156712852db17fd266cb403
Mukul <mukul1904@gmail.com>
2017-11-26 19:12:59 +0530

    Added messages

--- messages.js
+++ messages.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-    'titleMessage': '',
-    'greetingMessage': '',
-    'repromptMessage': '',
-    'goodByeMessgae': '',
-    'helpMessage': ''
+    'titleMessage': 'Welcome to AWS Delhi Skill',
+    'greetingMessage': 'Welcome to AWS Delhi Skill',
+    'repromptMessage': 'Please repeat',
+    'goodByeMessgae': 'Good Bye',
+    'helpMessage': 'Say the name of twitter handle, you want to get latest tweet of'
 }
